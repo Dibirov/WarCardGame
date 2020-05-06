1f4c7b83e8ef3ebdce344319cba6bcf0c9002914
Magomed Dibirov <kykikok@icloud.com>
2020-05-06 15:07:21 +0500

    FV

---
+++ CardWarGame/ViewController.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
+    
+    override func viewDidLoad() { super.viewDidLoad() }
+
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        // Рандом
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+        
+        // Обновляем картинки
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        // Сравниваем рандомные числа
+        if leftNumber > rightNumber {
+            
+            // Левый выигрывает
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftNumber < rightNumber {
+            
+            // Правый выиграывает
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+        else {
+            //
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+}
+
